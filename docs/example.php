931829a0a7c1fa79eda5899b325124eb2687e52e
till <till@php.net>
2009-12-14 18:06:33 +0000

    added example for Zend_Http_Client

--- docs/example.php
+++ docs/example.php
@@ -8,6 +8,19 @@ Services_Hoptoad::installHandlers("YOUR_HOPTOAD_API_KEY");
 // standalone
 require_once 'Services/Hoptoad.php';
 
+Services_Hoptoad::$apiKey = "YOUR_HOPTOAD_API_KEY";
+
+$exception = new Custom_Exception('foobar');
+Services_Hoptoad::handleException($exception);
+?>
+
+<?php
+// use Zend_Http_Client
+require_once 'Services/Hoptoad.php';
+
+Services_Hoptoad::$apiKey = "YOUR_HOPTOAD_API_KEY";
+Services_Hoptoad::$client = "Zend";
+
 $exception = new Custom_Exception('foobar');
 Services_Hoptoad::handleException($exception);
 ?>

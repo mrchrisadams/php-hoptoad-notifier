049cedbdc25f9d09c917b3912396391f49e62ccc
Till Klampaeckel <till@php.net>
2010-01-27 15:40:37 +0100

    extended test, waiting on hoptoad

--- tests/Services/HoptoadTest.php
+++ tests/Services/HoptoadTest.php
@@ -17,12 +17,27 @@ require_once 'PHPUnit/Framework/TestCase.php';
 
 class Services_HoptoadTest extends PHPUnit_Framework_TestCase
 {
+    protected $apiKey;
+
+    public function setUp()
+    {
+        $conf = dirname(dirname(__FILE__)) . '/test-config.php';
+        if (!file_exists($conf)) {
+            $this->fail("test-config.php is required to run this.");
+        }
+        include $conf;
+        if (empty($API_KEY)) {
+            $this->fail("The \$API_KEY in test-config.php cannot be empty.");
+        }
+        $this->apiKey = $API_KEY;
+    }
+
     /**
      * @expectedException RuntimeException
      */
     public function testOldApi()
     {
-        Services_Hoptoad::notify('1234', 'Lorem ipsum', 'foobar.php', 0, array());
+        Services_Hoptoad::notify($this->apiKey, 'Lorem ipsum', 'foobar.php', 0, array());
     }
 
     /**
@@ -32,10 +47,17 @@ class Services_HoptoadTest extends PHPUnit_Framework_TestCase
      */
     public function testHoptoad()
     {
-        $this->markTestIncomplete("Need a test config, etc..");
+        // $this->markTestIncomplete("Need a test config, etc..");
 
         $exception = new Exception("This is a test.");
-        Services_Hoptoad::$apiKey = '1234';
-        Services_Hoptoad::notifyV2($exception);
+
+        $hoptoad = new Services_Hoptoad($this->apiKey);
+        try {
+            $status = $hoptoad->notifyV2($exception);
+            $this->assertTrue($status);
+        } catch (Exception $e) {
+            var_dump($e->getCode(), $e->getRequestData());
+            $this->fail($e->getMessage());
+        }
     }
 }
